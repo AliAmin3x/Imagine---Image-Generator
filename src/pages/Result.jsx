46a205731b54f96bfe6a5eb97ee8fff0d8685b0f
Ali Amin <ali.qwerty32a0@gmail.com>
2025-08-18 02:56:26 +0500

    Fixed Mobile Responsiveness in Results page

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -6,10 +6,15 @@ const Result = () => {
   return (
     <form className="flex flex-col min-h-[90vh] justify-center items-center">
       <div>
-        <div className="relative">
-          <img src={image} alt="" className="max-w-md rounded" />
+        <div className="relative w-full max-w-md">
+          <img
+            src={image}
+            alt=""
+            className="w-full h-auto rounded object-cover"
+          />
           <span className="absolute bottom-0 left-0 h-2 bg-gradient-to-r from-blue-600 to-purple-700 w-full transition-all duration-[10s]" />
         </div>
+
         <p>Loading...</p>
 
         {!isImageLoaded && (
@@ -17,11 +22,15 @@ const Result = () => {
             <input
               type="text"
               placeholder="Describe what you want to generate"
-              className="flex-1 bg-transparent outline-none ml-8 max-sm:w-20"
+              className="flex-1 bg-transparent outline-none ml-3 sm:ml-8 text-xs sm:text-sm max-sm:w-20"
             />
             <button
               type="submit"
-              className="text-white cursor-pointer bg-gradient-to-r from-blue-500 to-purple-600 hover:from-blue-600 hover:to-purple-700 px-5 sm:px-10 py-3 rounded-full transition duration-300"
+              className="text-white cursor-pointer bg-gradient-to-r from-blue-500 to-purple-600 hover:from-blue-600 hover:to-purple-700 
+               px-3 py-2 text-xs 
+               sm:px-6 sm:py-3 sm:text-sm 
+               md:px-10 md:py-3 md:text-base 
+               rounded-full transition duration-300"
             >
               Generate
             </button>
