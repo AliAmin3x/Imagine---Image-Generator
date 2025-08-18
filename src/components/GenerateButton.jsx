eca23d4dcb66d5e9384d05d1b445db02e7e0d115
Ali Amin <ali.qwerty32a0@gmail.com>
2025-08-19 01:32:44 +0500

    Front-end Completed

--- src/components/GenerateButton.jsx
+++ src/components/GenerateButton.jsx
@@ -1,20 +1,53 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../assets/assets";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+import { AppContext } from "../context/AppContext";
 
 const GenerateButton = () => {
+  const { user, setShowLogin } = useContext(AppContext);
+  const navigate = useNavigate();
+
+  const onClickHandler = () => {
+    // Scroll to top first
+    window.scrollTo({ top: 0, behavior: "smooth" });
+
+    setTimeout(() => {
+      if (user) {
+        navigate("/result");
+      } else {
+        setShowLogin(true);
+      }
+    }, 750);
+  };
+
   return (
-    <div className="pb-16 text-center">
+    <motion.div
+      initial={{ opacity: 0, y: 100 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      className="pb-16 text-center"
+    >
       <h1 className="text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold text-neutral-800 py-6 md:py-16">
         See the Magic. Try Now
       </h1>
-      <Link to={"/result"}>
-        <button className="m-auto cursor-pointer inline-flex items-center justify-center px-6 py-3 bg-gradient-to-r from-blue-500 to-purple-600 text-white font-semibold rounded-full shadow-lg hover:from-blue-600 hover:to-purple-700 transition duration-300">
-          Generate Image
-          <img src={assets.star_group} alt="" className="h-6" />
-        </button>
-      </Link>
-    </div>
+      <motion.button
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          default: { duration: 0 },
+          opacity: { delay: 0.8, duration: 1 },
+        }}
+        onClick={onClickHandler}
+        className="m-auto cursor-pointer inline-flex items-center justify-center px-6 py-3 bg-gradient-to-r from-blue-500 to-purple-600 text-white font-semibold rounded-full shadow-lg hover:from-blue-600 hover:to-purple-700 transition duration-300"
+      >
+        Generate Image
+        <img src={assets.star_group} alt="" className="h-6 ml-2" />
+      </motion.button>
+    </motion.div>
   );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,98 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../assets/assets";
-import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { AppContext } from "../context/AppContext";
+import { Navigate, useNavigate } from "react-router-dom";
 const Header = () => {
+  const { user, setShowLogin } = useContext(AppContext);
+  const navigate = useNavigate();
+  const onClickHandler = () => {
+    if (user) {
+      navigate("/result");
+    } else {
+      setShowLogin(true);
+    }
+  };
   return (
-    <div className="flex flex-col justify-center items-center text-center my-20">
-      <div className="text-stone-500 inline-flex text-center gap-2 bg-white px-4 py-2 rounded-full border border-neutral-500  ">
+    <motion.div
+      className="flex flex-col justify-center items-center text-center my-20"
+      initial={{ opacity: 0.2, y: 100 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+    >
+      <motion.div
+        className="text-stone-500 inline-flex text-center gap-2 bg-white px-4 py-2 rounded-full border border-neutral-500  "
+        initial={{ opacity: 0, y: -20 }}
+        transition={{ duration: 0.8, delay: 0.2 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
         <p>Best text to image Generator</p>
         <img src={assets.star_icon} alt="" />
-      </div>
+      </motion.div>
 
-      <h1 className="text-4xl max-w-[400px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center text-gray-800">
+      <motion.h1
+        className="text-4xl max-w-[400px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center text-gray-800"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2, delay: 0.4 }}
+      >
         Turn Text to <span className="text-blue-600">image</span>, in seconds.
-      </h1>
+      </motion.h1>
 
-      <p className="mt-5 max-w-xl mx-auto text-gray-600 text-sm sm:text-base">
+      <motion.p
+        className="mt-5 max-w-xl mx-auto text-gray-600 text-sm sm:text-base"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.8 }}
+      >
         Unleash your creativity with AI. Turn your imagination into visual art
         in seconds – just type, and watch the magic happen.
-      </p>
+      </motion.p>
 
-      <Link to={"/result"}>
-        <button className="cursor-pointer mt-4 inline-flex items-center justify-center px-6 py-3 bg-gradient-to-r from-blue-500 to-purple-600 text-white font-semibold rounded-full shadow-lg hover:from-blue-600 hover:to-purple-700 transition duration-300">
-          Generate Images
-          <img className="h-6" src={assets.star_group} alt="" />
-        </button>
-      </Link>
+      <motion.button
+        onClick={onClickHandler}
+        className="cursor-pointer mt-4 inline-flex items-center justify-center px-6 py-3 bg-gradient-to-r from-blue-500 to-purple-600 text-white font-semibold rounded-full shadow-lg hover:from-blue-600 hover:to-purple-700 transition duration-300"
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          default: { duration: 0 },
+          opacity: { delay: 0.8, duration: 1 },
+        }}
+      >
+        Generate Images
+        <img className="h-6" src={assets.star_group} alt="" />
+      </motion.button>
 
-      <div className="flex flex-wrap justify-center mt-16 gap-3">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 1 }}
+        className="flex flex-wrap justify-center mt-16 gap-3"
+      >
         {Array(6)
           .fill("")
           .map((item, index) => (
-            <img
+            <motion.img
+              whileHover={{ scale: 1.05, duration: 0.1 }}
               className="rounded hover:scale-105 transition-all duration-300 cursor-pointer max-sm:w-10"
               src={index % 2 === 0 ? assets.sample_img_1 : assets.sample_img_2}
               key={index}
               width={70}
             />
           ))}
-      </div>
-      <p className="mt-2 text-neutral-600">Generated Images from Imagify</p>
-    </div>
+      </motion.div>
+      <motion.p
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.2, delay: 0.8 }}
+        className="mt-2 text-neutral-600"
+      >
+        Generated Images from Imagify
+      </motion.p>
+    </motion.div>
   );
 };
 
